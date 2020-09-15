fe2c24efe95310b7284e19b8a9c4f99352df44e2
eim5178 <eim5178@psu.edu>
2020-09-15 21:04:01 +0000

    changed function name

--- main.py
+++ main.py
@@ -11,18 +11,18 @@ def sum_n(n):
   else:
     return n + sum_n(n-1)
 
-def print_s(s,n):
+def print_n(s,n):
   if (n<=1):
     print(f"{s}")
   else:
     print(f"{s}")
-    n = print_s (s, n-1)
+    n = print_n (s, n-1)
 
 def run():
   n = int(input("Enter an int: "))
   print(f"sum is {sum_n(n)}")
   s = input("Enter an string: ")
-  print_s(s,n)
+  print_n(s,n)
 
 if __name__ == '__main__':
   run()
